7a3fb3053e010a6a0d89014e7d02a1d9762feea7
jagdish gupta <varshneyjacky@gmail.com>
2024-12-09 16:42:44 +0530

    configuring and creating student class

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name, address,college_name, email;
+    public Student(int id, String name, String address, String college_name, String email) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.college_name = college_name;
+        this.email = email;
+    }
+public Student(){
+
+}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCollege_name() {
+        return college_name;
+    }
+
+    public void setCollege_name(String college_name) {
+        this.college_name = college_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", college_name='" + college_name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
